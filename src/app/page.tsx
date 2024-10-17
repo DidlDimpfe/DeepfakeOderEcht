62781e8f047a60b858ec0cfe8e3da7babe1835cd
DidlDimpfe <philwojczak@gmail.com>
2024-10-17 21:22:29 +0200

    Really little bug fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,10 @@
 import { getRandomQuestionId } from "@/lib/queries";
-import { revalidatePath } from "next/cache";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
 export const dynamic = "force-dynamic";
-export const cache = "no-store";
-export const revalidate = 0;
 
 export default async function Page() {
-  revalidatePath("/");
-
   const userToken = cookies().get("userToken")?.value;
 
   if (!userToken) throw new Error("User token not found");
